126f90545d8d4e76ee4cff3f197adf404b0974d7
Stephen Holsenbeck <stephen@virgalabs.io>
2023-06-03 18:02:39 -0400

    add disable autoload again

--- app.R
+++ app.R
@@ -2,6 +2,6 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+options( "golem.app.prod" = TRUE, shiny.autoload.r = FALSE)
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
 quantifiedSelf::run_app() # add parameters here (if any)
